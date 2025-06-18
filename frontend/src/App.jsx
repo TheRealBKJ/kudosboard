5683a48883011065af5b85f75f3f5c73093a7dda
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-18 12:43:54 -0700

    added functions for app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,35 @@
 import './App.css'
 import Header from "./components/Header/Header"; //header with search and filter
 import BoardList from "./components/BoardList/BoardList" // boardlist with add new card, board card
+import Footer from './components/Footer/Footer';
 
 //link to boards view through clikcing on boards
 
 export default function App() {
 
   // fetch all boards using useEffect and passes down to BoardList
+  const fetchAll = () =>{
+
+  }
 
   //delete a board
+  const deleteBoard = (id) =>{
 
+  }
 
   // add a board
+
+  const addABoard = (data) =>{
+
+  }
+
+  return (
+    <div className='all-container'>
+      <Header/>
+      <BoardList/>
+      <Footer/>
+    </div>
+  )
 
 
 }
--- frontend/src/components/BoardList/BoardCard/BoardCard.jsx
+++ frontend/src/components/BoardList/BoardCard/BoardCard.jsx
@@ -1,36 +1,32 @@
-import{Link} from "react-router" // link to cards asosciated 
+import { Link } from "react-router" // link to cards asosciated 
 
 
 
-export default function BoardCard ({data , onDeleteBoard, onViewBoard}){
+export default function BoardCard({ data, onDeleteBoard}){
 
     return (
-        <div className = "movie-card">
-            <img className = "movie-card-img"/>
+        <div className="movie-card">
+            <img className="movie-card-img" />
 
             <div className="movie-card-contents">
-                <h2 className= "movie-card-title">{data.title}</h2>
-                <h3 className ="movie-card-genre"> {data.category}</h3>
+                <h2 className="movie-card-title">{data.title}</h2>
+                <h3 className="movie-card-genre"> {data.category}</h3>
                 <div className="movie-card-buttons">
-                    <button 
-                        className="view-button" 
-                        onClick ={() => onViewBoard(data.id)}>
-                        View Board
-                    </button>
 
+                    <Link
+                        className="cards-link"
+                        to={`/boards/${data.id}`}
+                        state= {{ title: data.title, category: data.category }} // use useLocation for cardLocation
+                    >
+                        View Board
+                    </Link>
                     <button
-                        className = "delete-button">
+                        className="delete-button">
                         OnClick = {() => onDeleteBoard(data.id)}
                         Delete Board
                     </button>
                 </div>
-
-
-
             </div>
-
         </div>
-
     );
-
 }
