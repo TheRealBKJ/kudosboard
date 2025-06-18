9b5d6b23e569f39612f557986602f05e927cacf5
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-18 09:35:43 -0700

    everything before migration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors)
 
 app.use(express.json()); //to parse everything into json
 
